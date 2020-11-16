ef77cfb8a90204db8f07a9fc139db2b733926295
andre.areias <47741081+andreareias@users.noreply.github.com>
2020-11-16 23:59:57 +0000

    Added Migrations to Migrations project

--- src/UI/MainWindow.xaml.cs
+++ src/UI/MainWindow.xaml.cs
@@ -1,18 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Services;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace UI
 {
@@ -22,7 +9,7 @@ namespace UI
     public partial class MainWindow : Window
     {
 
-        public MainWindow()
+        public MainWindow(IUserService service)
         {
             InitializeComponent();
         }
